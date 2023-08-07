aa315a1ed4fe1f634275df97d437f68557f20a65
mxcrpntr <carpenter.max@gmail.com>
2023-08-06 21:26:26 -0400

    first commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets')
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
+
+module.exports = app;
