bfb61248ff841b1975c3a2049c69f2027da18b4a
mxcrpntr <carpenter.max@gmail.com>
2023-08-06 22:17:33 -0400

    sixth commit

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model('User');
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
@@ -54,5 +56,19 @@ router.post('/register', async (req, res, next) => {
     })
   });
 });
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json({ user });
+  })(req, res, next);
+});
+
 
 module.exports = router;
